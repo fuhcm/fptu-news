1c7815553d99ef6417042bf2a7c4b176b112bcf8
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-02-21 17:09:34 +0700

    add news page

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,17 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>FPTU</span>
+        <span class="font-weight-light">NEWS</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn flat href="https://fptu.tech" target="_blank">
+        <span class="mr-2">Homepage</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld />
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      //
-    };
-  }
-};
-</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,74 @@
 <template>
-  <HelloWorld />
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3 mt-3>
+      <p v-if="loading">Loading...</p>
+      <p v-if="errored">Error!</p>
+
+      <v-card class="card-item" v-for="post in posts" v-bind:key="post.guid">
+        <v-img :src="post.thumbnail">
+          <v-container fill-height fluid pa-2>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <a :href="post.guid" target="_blank">
+                  <span
+                    class="headline white--text black"
+                    v-text="post.title"
+                  ></span>
+                </a>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-img>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>favorite</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>bookmark</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>share</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import axios from "axios";
 
 export default {
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: [],
+      loading: true,
+      errored: false
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.fptu.tech/crawl/medium")
+      .then(response => {
+        this.posts = response.data;
+        this.loading = false;
+      })
+      .catch(error => {
+        this.errored = true;
+        this.loading = false;
+        console.log(error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 };
 </script>
+
+<style>
+.card-item {
+  margin-bottom: 1rem;
+}
+</style>
